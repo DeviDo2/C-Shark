3cfe2693ca43416e8b5257d108e4000076632432
DeVera2 <rjdeverayt2@gmail.com>
2024-11-18 17:28:43 +0800

    Added Staff Dashboard

---
+++ Dashboard_STAFF/Dashboard/Form1.cs
@@ -0,0 +1,65 @@
+namespace Dashboard
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void home_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inventory_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemGrp_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salesOrders_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salesReturns_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void purchaseOrders_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reports_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void salesAct_groupBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void search_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notify_pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
